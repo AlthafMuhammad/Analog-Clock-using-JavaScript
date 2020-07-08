e4666546bc9ddfdd48beec6f03122192b1af58f9
*Althaf <althafmuhammad@computer.org>
2020-07-08 10:31:27 +0530

    Display Current Time

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPos=20;
-let minPos=130;
-let secPos=267;
+var date=new Date();
+let hr=date.getHours();
+let min=date.getMinutes();
+let sec=date.getSeconds();
+
+let hrPos=(hr*360/12)+(min*(360/60)/12);
+let minPos=(min*360/60)+(sec*(360/60)/60);
+let secPos=sec*360/60;
 
 HOURHAND.style.transform="rotate("+hrPos+"deg)";
 MINUTEHAND.style.transform="rotate("+minPos+"deg)";
