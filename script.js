81e76b7a73579fccbb4e76cc600ad792ee3435c4
*Althaf <althafmuhammad@computer.org>
2020-07-08 10:14:37 +0530

    Added the rotation of hands

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPos=20;
+let minPos=130;
+let secPos=267;
+
+HOURHAND.style.transform="rotate("+hrPos+"deg)";
+MINUTEHAND.style.transform="rotate("+minPos+"deg)";
+SECONDHAND.style.transform="rotate("+secPos+"deg)";
+
