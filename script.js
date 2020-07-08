7a31e9cb79281a5efb9953c6c04bd51152b45364
*Althaf <althafmuhammad@computer.org>
2020-07-08 10:34:33 +0530

    Moving Clock

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-var date=new Date();
-let hr=date.getHours();
-let min=date.getMinutes();
-let sec=date.getSeconds();
+function runTheClock(){
+    var date=new Date();
+    let hr=date.getHours();
+    let min=date.getMinutes();
+    let sec=date.getSeconds();
 
-let hrPos=(hr*360/12)+(min*(360/60)/12);
-let minPos=(min*360/60)+(sec*(360/60)/60);
-let secPos=sec*360/60;
+    let hrPos=(hr*360/12)+(min*(360/60)/12);
+    let minPos=(min*360/60)+(sec*(360/60)/60);
+    let secPos=sec*360/60;
 
-HOURHAND.style.transform="rotate("+hrPos+"deg)";
-MINUTEHAND.style.transform="rotate("+minPos+"deg)";
-SECONDHAND.style.transform="rotate("+secPos+"deg)";
+    HOURHAND.style.transform="rotate("+hrPos+"deg)";
+    MINUTEHAND.style.transform="rotate("+minPos+"deg)";
+    SECONDHAND.style.transform="rotate("+secPos+"deg)";
+    }
 
+var interval=setInterval(runTheClock,1000);
